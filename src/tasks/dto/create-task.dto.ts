163d62a473614207aa3e1cae593984b67ec09364
Wesley Gado <wesleygado@MacBook-Air-de-Wesley.local>
2023-04-27 17:47:33 -0300

    Utilizando Validação

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,15 @@
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'Nome deve ser uma string' })
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
   nome: string;
+
+  @IsString({ message: 'Descrição deve ser uma string' })
+  @IsNotEmpty({ message: 'Descrição não pode ser vazio' })
   descricao: string;
+
+  @IsBoolean({ message: 'Status deve ser do tipo boolean' })
+  @IsNotEmpty({ message: 'Status não pode ser vazio' })
   status: boolean;
 }
